32738e213f71c3c5ffb1c1bfaf6a96f1642190a7
akmalzaj <akmalzaj@gmail.com>
2015-09-06 01:22:10 +0800

    uppdate login

---
+++ app/controllers/users.rb
@@ -0,0 +1,41 @@
+enable :sessions
+
+get '/login' do
+	erb :login
+end
+
+get '/logout' do
+	session.clear
+	redirect '/'
+end
+
+get '/hacker_news' do
+	erb :hacker_news
+end
+
+post '/hacker_news' do
+	@user = User.authenticate(params[:user][:name], params[:user][:password])
+	if @user
+		session[:user_id] = @user.id
+		redirect '/hacker_news'
+	else
+	@login_errors = "Authentication failed. Check your details and try again"
+	redirect 'login'
+	end
+end
+
+get '/signup' do
+	erb :login
+end
+
+post '/signup' do
+	usr = User.find_by(name: params[:user][:name])
+    if usr.nil?
+        @user = User.create(params[:user])
+        redirect '/login'
+        @msg = "Successfully signup. Please login."
+    else
+        @signup_errors = "Email is already in the system. Try another email"
+        redirect '/signup'
+    end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,18 @@ class User < ActiveRecord::Base
   # Remember to create a migration!
   has_many :posts
   has_many :comments
+
+  def self.authenticate(name, password)
+    @user = User.find_by_name(name)
+
+    return false if @user.nil? 
+    # nil? → true; Only the object nil responds true to nil?.
+    # this is to say if the obj does not exist, 
+    # it will return true to nil?
+    if @user.password == password
+      return @user
+    else
+      return false
+    end
+  end
 end
